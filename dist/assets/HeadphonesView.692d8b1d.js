7ede1b5f8f6eb28caaec9e186dd05b0ba507b559
Adrien Charpentier <adriencharpentier@adriens-macbook.home>
2022-11-14 11:56:51 +0100

    deployment in gh-pages branch

---
+++ dist/assets/HeadphonesView.692d8b1d.js
@@ -0,0 +1,1 @@
+import{u as r,r as s,o as t,c as a,a as c}from"./index.668854a4.js";import{_ as n}from"./CategorySection.444edcbf.js";const m={__name:"HeadphonesView",setup(p){const e=r(),o=s("Headphones");return(u,_)=>(t(),a(n,{products:c(e).getHeadphones,category:o.value},null,8,["products","category"]))}};export{m as default};
