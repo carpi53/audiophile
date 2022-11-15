b06585c8b9c9339a938dce758eddf00427948ec7
Adrien Charpentier <adriencharpentier@adriens-macbook.home>
2022-11-15 11:05:48 +0100

    add favicon.ico

---
+++ assets/SpeakersView.c92524e0.js
@@ -0,0 +1,1 @@
+import{u as o,r as s,o as t,c as a,a as c}from"./index.a2295c68.js";import{_ as p}from"./CategorySection.7ee4fad2.js";const l={__name:"SpeakersView",setup(u){const e=o(),r=s("Speakers");return(n,_)=>(t(),a(p,{products:c(e).getSpeakers,category:r.value},null,8,["products","category"]))}};export{l as default};
