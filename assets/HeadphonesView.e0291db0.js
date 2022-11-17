8b7d62b6e7920d49bb91e4ec326dcdb9bec6230c
Adrien Charpentier <adriencharpentier@adriens-macbook.home>
2022-11-17 10:37:42 +0100

    change px with rem, fix header landmark

---
+++ assets/HeadphonesView.e0291db0.js
@@ -0,0 +1,1 @@
+import{u as r,r as s,o as t,c as a,a as c}from"./index.737732a8.js";import{_ as n}from"./CategorySection.5151ebda.js";const m={__name:"HeadphonesView",setup(p){const e=r(),o=s("Headphones");return(u,_)=>(t(),a(n,{products:c(e).getHeadphones,category:o.value},null,8,["products","category"]))}};export{m as default};
